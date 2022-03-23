96ba4c44ecf3c58290260e350abbf75768335ef4
Mat08251 <duguet@simplon-charleville.fr>
2022-03-23 16:35:44 +0100

    système de formulaire fait

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contactez-nous</h1>
+    <form method="post">
+        <label>Nom</label>
+        <input type="text" name="nom" required>
+        <label>Email</label>
+        <input type="email" name="email" required>
+        <label>Sujet</label>
+        <input type="text" name="sujet" required>
+        <label>Message</label>
+        <textarea name="message" cols="30" rows="10" required></textarea>
+        <input type="submit" value="Envoyer le message">
+    </form>
 <?php
-    if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['tel']) && !empty($_POST['email']) && !empty($_POST['message'])) {
-        $nom = htmlspecialchars($_POST('nom'));
-        $prenom = htmlspecialchars($_POST('prenom'));
-        $tel = htmlspecialchars($_POST('tel'));
-        $email = htmlspecialchars($_POST('email'));
-        $message = htmlspecialchars($_POST('message'));
+if (isset($_POST["message"])) {
+    $message = "Ce message vous a été envoyé via la page contact du site MD
+    Nom : " . $_POST["nom"] . "
+    Email : " . $_POST["email"] . "
+    Message : " . $_POST["message"];
 
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+   $retour = mail("bibi080@live.fr", $_POST["sujet"], $message, "Reply-to:" . $_POST["email"]);
+   if ($retour) {
+       echo "<p>L'email a bien été envoyé.</p>";
+   }
+}
+   ?>
+</body>
+</html>
 
-        }else{
-            echo "Email non valide";
-        }
-
-    }else{
-        header('Loction:Contact.html');
-    }
