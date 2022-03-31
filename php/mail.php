3d202c5345bb175ba9f7edd8ffbb359337b9db79
Mat08251 <duguet@simplon-charleville.fr>
2022-03-31 15:32:05 +0200

    verification phph mail fonctionnel

--- php/mail.php
+++ php/mail.php
@@ -1,17 +1,28 @@
 <?php
-if (isset($_POST["message"]) && ($_POST["nom"])) {
-    $message = "Ce message vous a été envoyé via la page contact du site MD
-    Nom : " . $_POST["nom"] . "
-    Prenom : " . $_POST["prenom"] . "
-    Email : " . $_POST["email"] . "
-    Tel : " . $_POST["tel"] . "
-    Message : " . $_POST["message"];
-    $sujet = "Salut";
+if (!empty($_POST["message"]) AND !empty($_POST["nom"])) {
 
-   $retour = mail("bibi080@live.fr", $sujet, $message, "Reply-to:" . $_POST["email"]);
-   if ($retour) {
-       echo "<p>L'email a bien été envoyé.</p>";
-   }
+    $nom = htmlspecialchars($_POST['nom']);
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $email = htmlspecialchars($_POST['email']);
+    $tel = htmlspecialchars($_POST['tel']);
+    $message = htmlspecialchars($_POST['message']);
+
+
+    if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)) {
+
+            $titre = "Ce message vous a été envoyé via la page contact du site MD
+            Nom : " . $_POST["nom"] . "
+            Prenom : " . $_POST["prenom"] . "
+            Email : " . $_POST["email"] . "
+            Tel : " . $_POST["tel"] . "
+            Message : " . $_POST["message"];
+            $sujet = "Salut";
+
+        $retour = mail("bibi080@live.fr", $sujet, $titre, "Reply-to:" . $_POST["email"]);
+        if ($retour) {
+            echo "<p>L'email a bien été envoyé.</p>";
+        }
+    }
 }
    ?>
 
